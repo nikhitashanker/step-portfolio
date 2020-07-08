df5f49b6dea9a7e6eedf15b828aa0660bbed1f79
Nikhita Shanker <rocketns@gmail.com>
2020-07-08 00:18:14 +0000

    Add servlets that return urls for login and logout.

---
+++ portfolio/src/main/java/com/google/sps/servlets/LogoutServlet.java
@@ -0,0 +1,18 @@
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout-url")
+public class LogoutServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html");
+    String logoutUrl =
+        UserServiceFactory.getUserService().createLogoutURL(/* urlToRedirectToAfterLogin= */ "/");
+    response.getWriter().println(logoutUrl);
+  }
+}
